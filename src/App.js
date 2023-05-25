703fc1c86eab99b84375ec2facc14061bf7123b0
André Oliveira <dev1@visonet.net>
2023-05-25 09:13:26 -0300

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [count, setCount] = useState(0);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Contador de Olá Mundos: {count}</h1>
+        <button onClick={() => setCount(count + 1)}>Aumentar</button>
       </header>
     </div>
   );
 }
+
 
 export default App;
